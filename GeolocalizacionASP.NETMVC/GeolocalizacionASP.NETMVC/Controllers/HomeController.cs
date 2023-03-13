7f457dee05cb52fd66b3f1d60d28c2f279616216
Ruben Celis <sistemas.celis@gmail.com>
2023-03-12 19:30:07 -0500

    Se crea el proyecto

---
+++ GeolocalizacionASP.NETMVC/GeolocalizacionASP.NETMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using GeolocalizacionASP.NETMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GeolocalizacionASP.NETMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region [Properties]
+        private readonly ILogger<HomeController> _logger;
+        #endregion
+
+        #region [Constrcutor]
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        #endregion
+
+        #region [ActionResult]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        } 
+        #endregion
+    }
+}
---
+++ GeolocalizacionASP.NETMVC/GeolocalizacionASP.NETMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeolocalizacionASP.NETMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
